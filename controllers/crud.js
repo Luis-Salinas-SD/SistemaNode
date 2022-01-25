eb3ec73bc2264b69f8f5e7e5f55bd1cba6b91f85
Luis-Salinas-SD <paus.impala21@gmail.com>
2022-01-24 23:05:38 -0600

    modulo agregra listo

--- controllers/crud.js
+++ controllers/crud.js
@@ -1,9 +1,15 @@
-//const { response } = require('express');
+
 const conexion = require('../database/db');
 
 exports.save = (request, response) => {
     const user = request.body.user;
     const rol = request.body.rol;
 
-    console.log('Bienvenido' + user + ' ' + rol);
+    conexion.query('INSERT INTO usuarios SET ?', { usuario: user, rol: rol }, (error, result) => {
+        if (error) {
+            console.error(error);
+        } else {
+            response.redirect('/')
+        }
+    })
 }
--- index.js
+++ index.js
@@ -4,14 +4,14 @@ const app = express();
 //! Invocar el motor de plantillas EJS
 app.set('view engine', 'ejs')
 
+//! Establecer el como recibir variablñes
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+//* Ruta
+app.use('/', require('./router'));
+
 //# Server
 app.listen(5000, () => {
     console.log('Runing Server LS | http://localhost:5000');
 })
-
-//! Establecer el como recibir variablñes
-app.use(express.urlencoded({ extended: false }));
-app.use(express(json));
-
-//* Ruta
-app.use('/', require('./router'));
