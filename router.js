819b51502dfbdc4325d7ecd62e031149bf073e53
Luis-Salinas-SD <paus.impala21@gmail.com>
2022-01-26 00:04:27 -0600

    Modulo edit casi listo

--- router.js
+++ router.js
@@ -3,25 +3,36 @@ const router = express.Router();
 
 const conexion = require('./database/db')
 
-/*! Mostrar Registros*/
+//! Mostrar Registros
 router.get('/', (request, response) => {
     conexion.query('SELECT * FROM usuarios', (error, results) => {
         if (error) {
             throw error;
         } else {
-            //response.send(results)
             response.render('index', { results: results })
         }
     })
 
 })
-/* Crear Registros */
+//* Crear Registros
 router.get('/create', (request, response) => {
     response.render('create');
 })
 
+router.get('/edit/:id', (request, response) => {
+    const id = request.params.id;
+    conexion.query('SELECT * FROM usuarios WHERE id=?', [id], (error, results) => {
+        if (error) {
+            throw error;
+        } else {
+            response.render('edit', { user: results[0] })
+        }
+    })
+})
+
 
 const crud = require('./controllers/crud');
+const req = require('express/lib/request');
 router.post('/save', crud.save)
 
 module.exports = router;
