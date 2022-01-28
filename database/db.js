54a2ebac9e90ee0d1359e45f177d0b535c3e602d
Luis-Salinas-SD <paus.impala21@gmail.com>
2022-01-27 22:49:48 -0600

    Actualizacion

--- database/db.js
+++ database/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 const conexion = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '123456',
+    password: '',
     database: 'crud_node',
     port: 3306
 })
